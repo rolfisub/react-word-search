ff4220135a40b059e78f32aabcb5f4de586fb3ca
Rolf Bansbach <rolfisub@gmail.com>
2018-10-23 23:10:02 -0400

    adding some more deps

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-word-search
 a reacty version of the word search game
+
+to start developemnt run: <code>yarn dev</code>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-const Index = () => <div>indexs</div>;
+const Index = () => <div>is this react?</div>;
 
 ReactDOM.render(<Index />, document.getElementById("root"));
