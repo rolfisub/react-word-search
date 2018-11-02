a982432cb84c916c257de36b555fec3e43f89478
Rolf Bansbach <rolfisub@gmail.com>
2018-11-02 10:06:21 -0400

    working on form part 1

--- src/game/components/generate.form.tsx
+++ src/game/components/generate.form.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { InjectedFormProps, reduxForm } from "redux-form";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
 import { Wordsearch, WordsearchInput } from "../wordsearch/wordsearch";
+import { isRequired } from "../../common/ui.field.validators";
+import { renderTextField } from "../../common/render.fields";
 
 interface GenerateFormProps {
   ws: Wordsearch;
@@ -9,5 +11,46 @@ interface GenerateFormProps {
 class GenerateForm extends React.Component<
   GenerateFormProps & InjectedFormProps<WordsearchInput>
 > {
-  
+  submit = values => {
+    console.log(values);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.props.handleSubmit(this.submit)}>
+        <div>
+          <Field
+            name={"size"}
+            label={"Game Size"}
+            required={true}
+            validate={[isRequired]}
+            component={renderTextField as any}
+          />
+          <Field
+            name={"wordsConfig.amount"}
+            label={"Amount of words to generate"}
+            required={true}
+            validate={[isRequired]}
+            component={renderTextField as any}
+          />
+          <Field
+            name={"wordsConfig.minLength"}
+            label={"Minimum word length"}
+            required={true}
+            validate={[isRequired]}
+            component={renderTextField as any}
+          />
+          <Field
+            name={"wordsConfig.maxLength"}
+            label={"Maximum word length"}
+            required={true}
+            validate={[isRequired]}
+            component={renderTextField as any}
+          />
+
+
+        </div>
+      </form>
+    );
+  }
 }
