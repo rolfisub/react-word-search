b9fff56506395e000a3ffb2cdad73b1887d424e1
Rolf Bansbach <rolfisub@gmail.com>
2018-11-03 11:58:29 -0400

    working on layout

--- src/webgame/components/board.tsx
+++ src/webgame/components/board.tsx
@@ -1,27 +1,32 @@
 import * as React from "react";
 import { WordsearchOutput } from "../../lib/wordsearch/wordsearch";
 import { Cell } from "./cell";
-import { GenerateFormReduxForm } from "./generate.form";
+import { Grid } from "@material-ui/core";
 
 export class Board extends React.Component<WordsearchOutput> {
   render() {
     return (
-      <div>
-        <div style={{ width: this.props.board.length * 22 }}>
-          {this.props.board.map((arr, x) => {
-            return (
-              <span key={x}>
-                {arr.map((cell, y) => {
-                  return <Cell {...cell} key={y} />;
-                })}
-                <br />
-              </span>
-            );
-          })}
-        </div>
-        <p>{this.props.words.toString()}</p>
-        <GenerateFormReduxForm />
-      </div>
+      <Grid container>
+        <Grid item xs={12}>
+          <div style={{ width: this.props.board.length * 22, float: "left" }}>
+            {this.props.board.map((arr, x) => {
+              return (
+                <span key={x}>
+                  {arr.map((cell, y) => {
+                    return <Cell {...cell} key={y} />;
+                  })}
+                  <br />
+                </span>
+              );
+            })}
+          </div>
+          <div style={{ height: this.props.board.length * 22, float: "left", margin: 10 }}>
+            {this.props.words.map((w, i) => {
+              return <p key={i}>{w}</p>;
+            })}
+          </div>
+        </Grid>
+      </Grid>
     );
   }
 }
--- src/webgame/game.tsx
+++ src/webgame/game.tsx
@@ -3,6 +3,9 @@ import * as fetch from "isomorphic-fetch";
 import { config } from "../config";
 import { Wordsearch, WordsearchOutput } from "../lib/wordsearch/wordsearch";
 import { Board } from "./components/board";
+import { Grid } from "@material-ui/core";
+import { GenerateFormReduxForm } from "./components/generate.form";
+
 interface GameState {
   gameBoard: WordsearchOutput;
 }
@@ -33,16 +36,29 @@ export class Game extends React.Component<any, GameState> {
   };
 
   loadDictionary = async () => {
-    const response = await fetch(config.api + "ws/dictionary");
-    const words = await response.json();
-    this.ws.setConfig({
-      wordsConfig: {
-        dictionary: [...words]
-      }
-    });
+    try {
+      const response = await fetch(config.api + "ws/dictionary");
+      const words = await response.json();
+      this.ws.setConfig({
+        wordsConfig: {
+          dictionary: [...words]
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   render() {
-    return <Board {...this.state.gameBoard} />;
+    return (
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6}>
+          <Board {...this.state.gameBoard} />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          <GenerateFormReduxForm />
+        </Grid>
+      </Grid>
+    );
   }
 }
