6e96e70f30abd18282683a47a41ab4c72311faa0
Rolf Bansbach <rolfisub@gmail.com>
2018-11-04 15:06:19 -0500

    cell on hover color

--- src/webgame/components/cell.tsx
+++ src/webgame/components/cell.tsx
@@ -12,8 +12,44 @@ const styles: CSSProperties = {
   textAlign: "center"
 };
 
-export class Cell extends React.Component<CellInterface> {
+const stylesHover: CSSProperties = {
+  ...styles,
+  backgroundColor: "lightgreen"
+};
+
+interface CellState {
+  isHover: boolean;
+}
+
+export class Cell extends React.Component<CellInterface, CellState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isHover: false
+    };
+  }
+
+  mouseEnter = () => {
+    this.setState({
+      isHover: true
+    });
+  };
+
+  mouseLeave = () => {
+    this.setState({
+      isHover: false
+    });
+  };
+
   render() {
-    return <span style={styles}>{this.props.letter}</span>;
+    return (
+      <span
+        onMouseEnter={this.mouseEnter}
+        onMouseLeave={this.mouseLeave}
+        style={this.state.isHover ? stylesHover : styles}
+      >
+        {this.props.letter}
+      </span>
+    );
   }
 }
