09591b0270aee626b41bd969955b7ca0e8bdb0f8
Rolf Bansbach <rolfisub@gmail.com>
2018-11-12 14:29:41 -0500

    sentry integration

---
+++ src/server/config.ts
@@ -0,0 +1,4 @@
+export default {
+  port: 3001,
+  sentryDsn: "https://e4cf3275502c4af2aaf3fba03c3f9cdc@sentry.io/1321377"
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,17 +1,32 @@
+import config from "./config";
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as cors from "cors";
 import { ws } from "./ws";
+import * as Sentry from "@sentry/node";
+
+const sentryConfig = { dsn: config.sentryDsn };
+
+Sentry.init(sentryConfig);
 
 const app = express();
-
+app.use(Sentry.Handlers.requestHandler() as express.RequestHandler);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 //word search endpoint
-app.use('/ws', ws);
+app.use("/ws", ws);
 
-app.listen(3001, () => {
-  console.log("listening on port:", 3001);
+app.get("/test", (req, res) => {
+  try {
+    res.send("test");
+    throw new Error("test event");
+  } catch (e) {
+    Sentry.captureException(e);
+  }
+});
+
+app.listen(config.port, () => {
+  console.log("listening on port:", config.port);
 });
